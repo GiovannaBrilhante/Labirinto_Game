76266a4ea485a92ac787743ef7a254006a5e8d99
GeovanaSilva <geovanasilva.5567o@gmail.com>
2022-12-05 15:45:01 -0300

    falta a tela de abertura

--- jogo.py
+++ jogo.py
@@ -21,30 +21,33 @@ pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
-
 # Create the screen object
 # The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 #Define a Player object by extending pygame.sprite.Sprite
 #The surface drawn on the screen is now an attribute of 'player'
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
        super(Player, self).__init__()
        self.surf = pygame.image.load("assets/sergio.jpg").convert()
        self.surf.set_colorkey((255,255,255), RLEACCEL)
        self.rect= self.surf.get_rect()
+       self.rect.x = 0
+       self.rect.y = SCREEN_HEIGHT / 2
+       self.speed = 3
        
            # Move the sprite based on user keypresses
     def update(self, pressed_keys):
         if pressed_keys[K_UP]:
-            self.rect.move_ip(0, -5)
+            self.rect.move_ip(0, -self.speed)
         if pressed_keys[K_DOWN]:
-            self.rect.move_ip(0, 5)
+            self.rect.move_ip(0, self.speed)
         if pressed_keys[K_LEFT]:
-            self.rect.move_ip(-5, 0)
+            self.rect.move_ip(-self.speed, 0)
         if pressed_keys[K_RIGHT]:
-            self.rect.move_ip(5, 0)
+            self.rect.move_ip(self.speed, 0)
 
         # Keep player on the screen
         if self.rect.left < 0:
@@ -58,25 +61,59 @@ class Player(pygame.sprite.Sprite):
 
 player = Player()
 
+enemies = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
+running = True
 # Define the enemy object by extending pygame.sprite.Sprite
 # The surface you draw on the screen is now an attribute of 'enemy'
-
 
 class Parede(pygame.sprite.Sprite):
     def __init__(self, config, cor):
         super(Parede, self).__init__()
+        print("Parede X ",config[0],"Y ",config[1],"Larg ",config[2],"Alt ",config[3])
+        print("Parede: " ,(config[2], config[3]), " Cor:" , cor)
         self.surf = pygame.Surface((config[2], config[3]))
         self.surf.fill(cor)
         self.rect = self.surf.get_rect()
+        self.rect.x = config[0]
+        self.rect.y = config[1]
 
     # Move the sprite based on speed
     # Remove the sprite when it passes the left edge of the screen
     def update(self):
         self.rect.move_ip(-self.speed, 0)
-        # if self.rect.right < 0:
-        # self.kill()
+        if self.rect.right < 0:
+           self.kill()
 
+azul = (0, 0, 255)
+vermelho = (255, 0, 0)
+verde = (0, 255, 0)
+rosa = (235, 0, 251)
+paredes_conf = [                  #                   sreem, cor(rgb), [rect.x,rect.y,larg, altura]       
+    [[400, 200, 200, 200], azul], # pygame.draw.rect(screen, (0, 0, 255), [400, 400, 200, 200])      
+    [[200, 400, 200, 200], verde],
+    [[200, 300, 200, 100], verde],
+    [[200, 000, 200, 100], verde],
+    [[600, 400, 200, 200], vermelho],
+    [[600, 300, 200, 100], vermelho],
+    [[600, 000, 200, 100], vermelho],
+    [[700, 000, 100, 200], vermelho],
+    [[700, 200, 100, 100], (0, 0,0)],
+    [[000, 400, 200, 200],  rosa],
+    [[000, 000, 200, 200],  rosa]
+]
 
+paredes = pygame.sprite.Group()
+# Adiciona paredes ao grupo
+for parede_conf in paredes_conf:
+    print("ParedeConf1: ", parede_conf[0], "ParedeCor: ",parede_conf[1])
+    parede = Parede(parede_conf[0], parede_conf[1])
+    paredes.add(parede)
+
+FPS = 60
+clock = pygame.time.Clock()
 running = True
 while running:
 
@@ -94,16 +131,23 @@ while running:
 
     # Update the player sprite based on user keypresses
     player.update(pressed_keys)
+    
+    enemies.update()
  # Fill the background with white
     screen.fill((255, 255, 255))
-
-    paredes_conf = [400, 200, 200, 200, (0, 0, 255)]
-    parede = Parede(paredes_conf, paredes_conf[4])
-    screen.blit(parede.surf, parede.rect)
+    # parede = Parede(paredes_conf, paredes_conf[4])
+    # screen.blit(parede.surf, parede.rect)
     #for parede_conf in paredes_conf:
+     #   parede = Parede(parede_conf[0], parede_conf[1])
 
+    # Exibe as paredes
+    for parede in paredes:
+        screen.blit(parede.surf, parede.rect)
 
-    '''pygame.draw.rect(screen, (0, 0, 255), [400, 200, 200, 200])
+    if pygame.sprite.spritecollideany(player, paredes):
+        player.kill()
+        running = False
+    '''
     pygame.draw.rect(screen, (0, 0, 255), [400, 400, 200, 200])
     # verde
     pygame.draw.rect(screen, (0, 255, 0), [200, 400, 200, 200])
@@ -118,13 +162,21 @@ while running:
     #pygame.draw.rect(screen, (0, 0,0), [700, 200, 100, 100])
     # rosa
     pygame.draw.rect(screen, (235, 0, 251), [000, 400, 200, 200])
-    pygame.draw.rect(screen, (235, 0, 251), [000, 000, 200, 200])'''
-
+    pygame.draw.rect(screen, (235, 0, 251), [000, 000, 200, 200])
+'''
     screen.blit(player.surf, player.rect)
+    for entity in all_sprites:
+          screen.blit(entity.surf, entity.rect)
 
 
-# Flip the display
+
+    if pygame.sprite.spritecollideany(player,enemies):
+        player.kill()
+        running = False
+
+ # Flip the display
     pygame.display.flip()
+    clock.tick(FPS)
 
 
 pygame.quit()
